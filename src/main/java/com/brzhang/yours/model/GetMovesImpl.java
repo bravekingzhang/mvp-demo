f310b790f805e43f37e38532edd1d213e0c91eb0
brhoolly <bravekingzhang@qq.com>
2015-12-27 15:46:08 +0800

    引入了Rxandroid框架，看了下，这个异步的例子好强大。感觉完全不用volley都OK。

--- src/main/java/com/brzhang/yours/model/GetMovesImpl.java
+++ src/main/java/com/brzhang/yours/model/GetMovesImpl.java
@@ -1,5 +1,9 @@
 package com.brzhang.yours.model;
 
+
+import android.nfc.Tag;
+import android.util.Log;
+
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
@@ -11,6 +15,13 @@ import com.google.gson.reflect.TypeToken;
 
 import java.util.List;
 
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.android.schedulers.HandlerScheduler;
+
 /**
  * Created by brzhang on 15/12/27.
  * Description :搬运工，你懂的，专门处理数据的模块，
@@ -18,8 +29,40 @@ import java.util.List;
  * 负责2，处理数据，格式化数据
  */
 public class GetMovesImpl implements GetMoves {
+    private static final String Tag = GetMovesImpl.class.getName();
     @Override
     public void getMovesFromNet(final OnFinishedListener listener) {
+
+        Observable.just("已", "经", "全", "部", "为","您","读","出","啦")
+                .filter(new Func1<String, Boolean>() {
+                    @Override
+                    public Boolean call(String s) {
+                        try {
+                            Thread.sleep(1000);
+                        } catch ( InterruptedException e ) {
+                            e.printStackTrace();
+                        }
+                        return true;
+                    }
+                })
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<String>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.e(Tag, "onCompleted");
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e(Tag, "onError");
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        listener.onError(s);
+                    }
+                });
         String request_url = "http://test.igame.qq.com/am/index.php?of=json";
 
         StringRequest stringRequest = new StringRequest(request_url, new Response.Listener<String>() {
